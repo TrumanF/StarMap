88245bc24ad26b12b4b68e2f5ded1174bb9a372e
TrumanF <trumanfarr@gmail.com>
2023-02-11 23:56:29 -0800

    2/11/2023 end of day

--- Chart.py
+++ Chart.py
@@ -10,17 +10,19 @@ import numpy as np
 from astropy.time import Time
 from astropy.coordinates import AltAz, EarthLocation, get_body
 from astropy import units as u
+import time
 
 
 def polar_to_cartesian(r, theta):
-    return r*math.cos(theta), r*math.sin(theta)
+    return r * math.cos(theta), r * math.sin(theta)
 
 
-# TODO: Add_star and add_sso are very similar, could be one function?
+# NOTE: Add_star and add_sso are very similar, could be one function?
 class Chart:
     def __init__(self, OBS_INFO, CANVAS_INFO, STAR_DATA):
+
         self.CANVAS_Y, self.CANVAS_X = CANVAS_INFO
-        self.chartSVG = SVG(self.CANVAS_Y, self.CANVAS_X)
+        self.chartSVG = SVG(self.CANVAS_Y, self.CANVAS_X)  # NOTE: Consider making this abstract
         self.CHART_ELEMENT_OPACITY = .25
         self.CHART_ELEMENT_WIDTH = 2
 
@@ -28,15 +30,19 @@ class Chart:
         self.AA = AltAz(location=self.OBS_LOC, obstime=self.OBS_TIME)  # AltAz frame from OBS_LOC and OBS_TIME
 
         self.star_list = []
-        self.min_mag = 0
-        self.max_mag = 0
         self.stars_above_horizon = []
         self.brightest_stars_list = []
+
+        time1 = time.time()
 
         self.STAR_DATA = STAR_DATA  # path (str) to star.csv
         star_df = pd.read_csv(STAR_DATA, keep_default_na=False, nrows=15000)  # Note: change this 15000 to some variable
         for i in star_df.index:
             self.add_star(Star(star_df['ra'][i], star_df['dec'][i], star_df['mag'][i], star_df['proper'][i]))
+
+        time2 = time.time()
+        print(f'Time: {time2-time1}')
+        print(len(self.star_list))
 
         self.min_star_size = .5
         self.max_star_size = 7
@@ -49,7 +55,6 @@ class Chart:
             self.add_sso(SSO(planet))
 
     def add_star(self, star):
-        star_mag = star.mag
         # Note: these lines could be moved to a general add_preprocess function, but not sure yet
         star_altaz_frame = star.coord.transform_to(self.AA)
         star.az = float(star_altaz_frame.az.to_string(unit=u.rad, decimal=True))
@@ -57,10 +62,6 @@ class Chart:
 
         if star.alt > 0:
             self.stars_above_horizon.append(star)
-        if star_mag > self.max_mag:
-            self.max_mag = star_mag
-        if star_mag < self.min_mag:
-            self.min_mag = star_mag
 
         self.star_list.append(star)
 
@@ -101,9 +102,11 @@ class Chart:
 class RadialChart(Chart):
     def __init__(self, OBS_INFO, CANVAS_INFO, STAR_DATA):
         super().__init__(OBS_INFO, CANVAS_INFO, STAR_DATA)
-        self.MAIN_CIRCLE_R = (self.CANVAS_Y * .9)/2
-        self.MAIN_CIRCLE_CX = self.CANVAS_X/2
-        self.MAIN_CIRCLE_CY = self.CANVAS_Y/2
+        self.MAIN_CIRCLE_R = (self.CANVAS_Y * .9) / 2  # NOTE: Change multiplier to parameter
+        self.SCALING_CONSTANT = self.MAIN_CIRCLE_R / 675.0  # Based on visual preference from other charts
+        self.CHART_ELEMENT_WIDTH *= self.SCALING_CONSTANT
+        self.MAIN_CIRCLE_CX = self.CANVAS_X / 2
+        self.MAIN_CIRCLE_CY = self.CANVAS_Y / 2
         # call function to create base chart
         self.add_base_elements()
 
@@ -111,7 +114,7 @@ class RadialChart(Chart):
         return f'RadialChart | Stars loaded: {len(self.star_list)} | SSOs loaded :{len(self.sso_list)}'
 
     def add_base_elements(self):
-        self.chartSVG.circle(self.MAIN_CIRCLE_CX, self.MAIN_CIRCLE_CY, self.MAIN_CIRCLE_R, "white", width=5, fill=False)
+        self.chartSVG.circle(self.MAIN_CIRCLE_CX, self.MAIN_CIRCLE_CY, self.MAIN_CIRCLE_R, "white", width=5*self.SCALING_CONSTANT, fill=False)
         # consider changing circle center to "50%" if I want to scale with canvas size, for now, OK
         # draw RA line markings at every hour
         ra_line_angles = np.linspace(0, 2 * math.pi, 25)  # 25 segments, so we can ignore the redundant 0 and 2pi
@@ -128,22 +131,21 @@ class RadialChart(Chart):
                                  width=self.CHART_ELEMENT_WIDTH, fill=False, opacity=self.CHART_ELEMENT_OPACITY)
 
         # print compass directions
-        self.chartSVG.text(self.MAIN_CIRCLE_CX, self.MAIN_CIRCLE_CY + self.MAIN_CIRCLE_R, "N", size=50,
-                           color="white", dx=-15, dy=20)
-        self.chartSVG.text(self.MAIN_CIRCLE_CX, self.MAIN_CIRCLE_CY - self.MAIN_CIRCLE_R, "S", size=50,
-                           color="white", dx=-15, dy=-50)
-        self.chartSVG.text(self.MAIN_CIRCLE_CX + self.MAIN_CIRCLE_R, self.MAIN_CIRCLE_CY, "W", size=50,
-                           color="white", dx=15, dy=-15)
-        self.chartSVG.text(self.MAIN_CIRCLE_CX - self.MAIN_CIRCLE_R, self.MAIN_CIRCLE_CY, "E", size=50,
-                           color="white", dx=-45, dy=-15)
+        text_size = 50 * self.SCALING_CONSTANT
+        self.chartSVG.text(self.MAIN_CIRCLE_CX, self.MAIN_CIRCLE_CY + self.MAIN_CIRCLE_R, "N", size=text_size,
+                           color="white", dx=-15*self.SCALING_CONSTANT, dy=20*self.SCALING_CONSTANT)
+        self.chartSVG.text(self.MAIN_CIRCLE_CX, self.MAIN_CIRCLE_CY - self.MAIN_CIRCLE_R, "S", size=text_size,
+                           color="white", dx=-15*self.SCALING_CONSTANT, dy=-50*self.SCALING_CONSTANT)
+        self.chartSVG.text(self.MAIN_CIRCLE_CX + self.MAIN_CIRCLE_R, self.MAIN_CIRCLE_CY, "W", size=text_size,
+                           color="white", dx=15*self.SCALING_CONSTANT, dy=-15*self.SCALING_CONSTANT)
+        self.chartSVG.text(self.MAIN_CIRCLE_CX - self.MAIN_CIRCLE_R, self.MAIN_CIRCLE_CY, "E", size=text_size,
+                           color="white", dx=-45*self.SCALING_CONSTANT, dy=-15*self.SCALING_CONSTANT)
 
         # draw magnitude legend
         # self.chartSVG.rect(50, self.CANVAS_Y - 50, 250, 325, fill="none", stroke_width=3, rx=2)
 
     def plot_preprocess_obj(self, cel_obj):
-        # mag_info right now is ONLY float of random size to plot object
         cel_obj.normalized_alt = -1 * (cel_obj.alt / 90 - 1)
-
         # stars with 90 should plot at center, stars with zero should be on edge, so normalize and reverse direction
         x, y = polar_to_cartesian((cel_obj.normalized_alt * self.MAIN_CIRCLE_R), -cel_obj.az + math.pi / 2)
         # -az because AZ increases to the east and add pi/2 to ensure north is up
@@ -155,32 +157,46 @@ class RadialChart(Chart):
         if SSOs:
             self.plot_ssos(SSOs)
 
-    def plot_star(self, star):
+    # TODO: Get rid of min and max_mag, get min_max mag once we know what subset we're plotting
+
+    def plot_star(self, star, mag_info):
         self.plot_preprocess_obj(star)
+        # Note: This works great, but if I wanted to plot again with different
+        #  visual parameters, I'd have to re-preprocess, which doesn't need to happen since the xy
+        #  coords would be the same
+        min_mag, max_mag = mag_info
         # make line below its own function?
         star.size = self.max_star_size * (
-                    1 - (star.mag - self.min_mag) / (self.max_mag - self.min_mag)) + self.min_star_size
+                1 - (star.mag - min_mag) / (max_mag - min_mag)) + self.min_star_size
+        star.size *= self.SCALING_CONSTANT
         self.chartSVG.circle(star.x, star.y, star.size, star.color, fill="url(#StarGradient1)", width=0)
 
     # TODO: Make better 'labels' flag, maybe can specify by filter which stars get labels
     # TODO: Change num_stars to be able to specify that you want all available stars to be plotted
     def plot_stars(self, num_stars=0, sort_filter=None, reverse_flag=False, labels=None):
         sorted_stars_to_plot = self.sorted_stars(sort_filter, reverse_flag)
+        # NOTE: This can be made faster in some scenarios where sorted_stars already is sorted by mag
+        sorted_stars_mag_sorted = [st.mag for st in sorted(sorted_stars_to_plot, key=lambda s: s.mag)]
+        min_mag = min(sorted_stars_mag_sorted)
+        max_mag = max(sorted_stars_mag_sorted)
         if num_stars > len(sorted_stars_to_plot):
             num_stars = len(sorted_stars_to_plot) - 1
             print(f"Number of stars requested is greater than available to plot, setting number to {num_stars}")
         for star in sorted_stars_to_plot[:num_stars]:
-            self.plot_star(star)
+            self.plot_star(star, (min_mag, max_mag))
         if labels:
             for star in sorted_stars_to_plot[:num_stars][:labels]:
-                self.chartSVG.text(star.x, star.y, star.name.capitalize(), color=star.color, dx=5 + star.size)
+                self.chartSVG.text(star.x, star.y, star.name.capitalize(), color=star.color, dx=5 + star.size,
+                                   size=15*self.SCALING_CONSTANT)
 
     def plot_sso(self, sso):
         self.plot_preprocess_obj(sso)
+        sso.size *= self.SCALING_CONSTANT
         self.chartSVG.circle(sso.x, sso.y, sso.size, sso.color, fill=sso.color)
 
         if sso.label_print:
-            self.chartSVG.text(sso.x, sso.y, sso.name.capitalize(), color=sso.color, dx=5 + sso.size)
+            self.chartSVG.text(sso.x, sso.y, sso.name.capitalize(), color=sso.color, dx=5 + sso.size,
+                               size=15*self.SCALING_CONSTANT)
 
     # TODO: Revisit the 'plotting_list', code seems a little funky
     def plot_ssos(self, plotting_list=True):
@@ -218,8 +234,8 @@ def test():
     OBS_LONG = -122.496369
     OBS_TIME = "13:00:00"
     OBS_DATE = "2023-02-11"
-    OBS_LOC = EarthLocation(lat=OBS_LAT, lon=OBS_LONG, height=100*u.m)
-    utcoffset = -8*u.hour
+    OBS_LOC = EarthLocation(lat=OBS_LAT, lon=OBS_LONG, height=100 * u.m)
+    utcoffset = -8 * u.hour
     OBS_TIME = Time(f'{OBS_DATE}T{OBS_TIME}') - utcoffset
 
     testRad = RadialChart((OBS_LOC, OBS_TIME), (1500, 1800), 'Star CSV/stars_labels.csv')
